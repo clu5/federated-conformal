94b4b3e6a2ff1789f12ff115e7b3f5329a8fe7ed
charlie <>
2023-01-24 18:06:59 -0500

    update cifar10-hetero

--- cifar10-hetero.sh
+++ cifar10-hetero.sh
@@ -11,10 +11,11 @@ source /etc/profile
 module load anaconda cuda
 
 
-#python src/run_TCT.py --dataset cifar10-2 --architecture small_resnet14 --rounds_stage1 200 --rounds_stage2 0 --local_epochs_stage1 5 --local_lr_stage1 0.01 --samples_per_client 10000 --momentum 0.9 --use_data_augmentation  --save_dir new_experiments --data_dir ../data
+python src/run_TCT.py --dataset cifar10-2 --architecture small_resnet14 --rounds_stage1 200 --rounds_stage2 0 --local_epochs_stage1 5 --local_lr_stage1 0.01 --samples_per_client 10000 --momentum 0.9 --use_data_augmentation  --save_dir new_experiments --data_dir ../data
 
 python src/run_TCT.py --dataset cifar10-2 --architecture small_resnet14 --rounds_stage1 100 --rounds_stage2 100 --local_epochs_stage1 5 --local_lr_stage1 0.01 --samples_per_client 10000 --local_steps_stage2 500 --local_lr_stage2 0.0001 --momentum 0.9 --use_data_augmentation --save_dir new_experiments --data_dir ../data --override
 
 python src/run_TCT.py --dataset cifar10-3 --architecture small_resnet14 --rounds_stage1 100 --rounds_stage2 100 --local_epochs_stage1 5 --local_lr_stage1 0.01 --samples_per_client 10000 --local_steps_stage2 500 --local_lr_stage2 0.0001 --momentum 0.9 --use_data_augmentation --save_dir new_experiments --data_dir ../data --override
 
-#python src/run_TCT.py --dataset cifar10 --architecture small_resnet14 --rounds_stage1 200 --rounds_stage2 0 --local_epochs_stage1 5 --local_lr_stage1 0.01 --samples_per_client 10000 --momentum 0.9 --use_data_augmentation  --save_dir new_experiments --data_dir ../data
+python src/run_TCT.py --dataset cifar10 --architecture small_resnet14 --rounds_stage1 100 --rounds_stage2 100 --local_epochs_stage1 5 --local_lr_stage1 0.01 --samples_per_client 10000 --local_steps_stage2 500 --local_lr_stage2 0.0001 --momentum 0.9 --use_data_augmentation --use_iid_partition --save_dir new_experiments --data_dir ../data --override
+
